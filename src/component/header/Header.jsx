9e359331e2b53297b21499b2f1a0cea510a8146d
Nitish Verma <nitishverma64@gmail.com>
2023-12-11 19:51:55 +0530

    responsive

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,20 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.png'
 import './Header.css'
+import {BiMenuAltRight} from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler'
+
 
 const Header = () => {
+  const [menuOpened,setMenuOpened] = useState(false);
+
+  const getMenuStyles=(menuOpened) =>{
+    if(document.documentElement.clientWidth <=800  ){
+      return {
+        right: !menuOpened && '-100%'
+      }
+    }
+  }
+
   return (
     <section className='head-wrapper'>
         <div className="paddings innerWidth flexCenter head-container">
           <img src={logo} alt="logo"  width={100}/>
-            <div className="flexCenter head-menu">
-                <a href="">Residencies</a>
-                <a href="">Our Value</a>
-                <a href="">Contact Us</a>
-                <a href="">Get Started</a>
-                <button className='button'> 
-                    <a href="">Contact</a>
-                </button>
+            <OutsideClickHandler 
+                onOutsideClick={()=>{
+                setMenuOpened(false)
+              }}>
+              <div className="flexCenter head-menu"
+                style={getMenuStyles(menuOpened)}>
+                    <a href="">Residencies</a>
+                    <a href="">Our Value</a>
+                    <a href="">Contact Us</a>
+                    <a href="">Get Started</a>
+                    <button className='button'> 
+                        <a href="">Contact</a>
+                    </button>
+              </div>
+            </OutsideClickHandler>
+            <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+              <BiMenuAltRight size={30}/>
             </div>
         </div>
     </section>
